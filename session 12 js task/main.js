fa580bbc29fd0dab2ab004827e3c346c26c95322
medo <moglal001@gmail.com>
2026-07-20 11:46:51 +0300

    task

---
+++ session 12 js task/main.js
@@ -0,0 +1,12 @@
+let ids = [10 ,40 ,50 ,82 ,33]
+let UserInput = Number(prompt("Enter ID"))
+let findIndex = ids.findIndex((item)=> item == UserInput);
+if(findIndex == -1)
+{
+    console.log("unfound");
+    ids.push(UserInput)
+}
+else{
+    console.log("found");
+    }
+console.log(ids);
